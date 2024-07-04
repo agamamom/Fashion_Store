3843b94581e14c76ce476f879c7c16da2fa07994
dattht.hiday <77887747+agamamom@users.noreply.github.com>
2024-07-04 21:05:08 +0700

    update code

--- lib/views/buyers/nav_screens/home_screen.dart
+++ lib/views/buyers/nav_screens/home_screen.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Home screen'),
+    return Padding(
+      padding: EdgeInsets.only(
+          top: MediaQuery.of(context).padding.top, left: 25, right: 15),
+      child: Row(
+        children: [
+          const Text(
+            'Howdy, What Are You\n Looking For 👀',
+            style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold),
+          ),
+          Container(
+            child: SvgPicture.asset('assets/icons/cart.svg'),
+            width: 20,
+          ),
+        ],
+      ),
     );
   }
 }
