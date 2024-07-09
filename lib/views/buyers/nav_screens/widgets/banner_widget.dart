6030254d1914e63e439465647fe44430d1cf5eda
dattht.hiday <77887747+agamamom@users.noreply.github.com>
2024-07-09 22:12:00 +0700

    update code

--- lib/views/buyers/nav_screens/widgets/banner_widget.dart
+++ lib/views/buyers/nav_screens/widgets/banner_widget.dart
@@ -5,10 +5,29 @@ class BannerWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 140,
-      width: MediaQuery.of(context).size.width,
-      decoration: BoxDecoration(color: Colors.yellow.shade900),
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Container(
+        height: 140,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.yellow.shade900,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: PageView(
+          children: const [
+            Center(
+              child: Text('Banner 1'),
+            ),
+            Center(
+              child: Text('Banner 2'),
+            ),
+            Center(
+              child: Text('Banner 3'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
