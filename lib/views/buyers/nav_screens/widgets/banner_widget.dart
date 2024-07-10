1bf9acbefca5527685bcb17c75c8a102a6461c4b
dattht.hiday <77887747+agamamom@users.noreply.github.com>
2024-07-10 22:06:46 +0700

    update code

--- lib/views/buyers/nav_screens/widgets/banner_widget.dart
+++ lib/views/buyers/nav_screens/widgets/banner_widget.dart
@@ -1,33 +1,33 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-class BannerWidget extends StatelessWidget {
-  const BannerWidget({super.key});
+// class BannerWidget extends StatelessWidget {
+//   const BannerWidget({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: Container(
-        height: 140,
-        width: double.infinity,
-        decoration: BoxDecoration(
-          color: Colors.yellow.shade900,
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: PageView(
-          children: const [
-            Center(
-              child: Text('Banner 1'),
-            ),
-            Center(
-              child: Text('Banner 2'),
-            ),
-            Center(
-              child: Text('Banner 3'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Padding(
+//       padding: const EdgeInsets.all(10.0),
+//       child: Container(
+//         height: 140,
+//         width: double.infinity,
+//         decoration: BoxDecoration(
+//           color: Colors.yellow.shade900,
+//           borderRadius: BorderRadius.circular(10),
+//         ),
+//         child: PageView(
+//           children: const [
+//             Center(
+//               child: Text('Banner 1'),
+//             ),
+//             Center(
+//               child: Text('Banner 2'),
+//             ),
+//             Center(
+//               child: Text('Banner 3'),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
