bf76cdbe5ec8f47c6519c269f62c573567ec89b0
dattht.hiday <77887747+agamamom@users.noreply.github.com>
2024-08-03 22:34:33 +0700

    update code

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,7 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthController {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   Future<String> signUpUsers(String email, String fullName, String phoneNumber,
       String password) async {
     String res = 'Some error occured';
@@ -12,6 +14,14 @@ class AuthController {
           password.isNotEmpty) {
         UserCredential cred = await _auth.createUserWithEmailAndPassword(
             email: email, password: password);
+
+        await _firestore.collection('buyers').doc(cred.user!.uid).set({
+          'email': email,
+          'fullName': fullName,
+          'phoneNumber': phoneNumber,
+          'buyerId': cred.user!.uid,
+          'address': '',
+        });
         res = 'success';
       } else {
         res = 'Please Fields must not be empty';
--- lib/views/buyers/auth/register_screen.dart
+++ lib/views/buyers/auth/register_screen.dart
@@ -1,27 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:multi_fashion_store/controllers/auth_controller.dart';
+import 'package:multi_fashion_store/utils/show_snackBar.dart';
 import 'package:multi_fashion_store/views/buyers/auth/login_screen.dart';
 
-class RegisterScreen extends StatelessWidget {
-  RegisterScreen(
-      {super.key,
-      required this.email,
-      required this.fullname,
-      required this.password,
-      required this.phoneNumber});
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({
+    super.key,
+  });
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
   final AuthController _authController = AuthController();
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   late String email;
   late String fullname;
   late String phoneNumber;
   late String password;
 
+  bool _isLoading = false;
+
   _signUpUser() async {
-    String res = await _authController.signUpUsers(
-        email, fullname, phoneNumber, password);
-    if (res != 'success') {
-      print(res);
+    setState(() {
+      _isLoading = true;
+    });
+    if (_formKey.currentState!.validate()) {
+      await _authController
+          .signUpUsers(email, fullname, phoneNumber, password)
+          .whenComplete(
+        () {
+          setState(() {
+            _formKey.currentState!.reset();
+            _isLoading = false;
+          });
+        },
+      );
+      return showSnack(
+          context, 'Congratulations An Account Has Been Created For You');
     } else {
-      print('Good');
+      setState(() {
+        _isLoading = false;
+      });
+      return showSnack(context, 'Please Fields must not be empty');
     }
   }
 
@@ -30,100 +53,136 @@ class RegisterScreen extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Create Customer Account',
-                style: TextStyle(
-                  fontSize: 20,
-                ),
-              ),
-              CircleAvatar(
-                radius: 64,
-                backgroundColor: Colors.yellow.shade900,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(13.0),
-                child: TextFormField(
-                  onChanged: (value) {
-                    email = value;
-                  },
-                  decoration: const InputDecoration(labelText: 'Enter Email'),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(13.0),
-                child: TextFormField(
-                  onChanged: (value) {
-                    fullname = value;
-                  },
-                  decoration:
-                      const InputDecoration(labelText: 'Enter Full Name'),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(13.0),
-                child: TextFormField(
-                  onChanged: (value) {
-                    phoneNumber = value;
-                  },
-                  decoration:
-                      const InputDecoration(labelText: 'Enter Phone Number'),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(13.0),
-                child: TextFormField(
-                  onChanged: (value) {
-                    password = value;
-                  },
-                  decoration: const InputDecoration(labelText: 'Password'),
-                ),
-              ),
-              GestureDetector(
-                onTap: () {
-                  _signUpUser();
-                },
-                child: Container(
-                  width: MediaQuery.of(context).size.width - 40,
-                  height: 50,
-                  decoration: BoxDecoration(
-                    color: Colors.yellow.shade900,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: const Center(
-                    child: Text(
-                      'Register',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 19,
-                          fontWeight: FontWeight.bold,
-                          letterSpacing: 4),
-                    ),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Create Customer Account',
+                  style: TextStyle(
+                    fontSize: 20,
                   ),
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text('Already Have An Account?'),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) {
-                            return const LoginScreen();
-                          },
-                        ),
-                      );
+                CircleAvatar(
+                  radius: 64,
+                  backgroundColor: Colors.yellow.shade900,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(13.0),
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please Email must not be empty';
+                      } else {
+                        return null;
+                      }
                     },
-                    child: const Text('Login'),
-                  )
-                ],
-              )
-            ],
+                    onChanged: (value) {
+                      email = value;
+                    },
+                    decoration: const InputDecoration(labelText: 'Enter Email'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(13.0),
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please Full Name must not be empty';
+                      } else {
+                        return null;
+                      }
+                    },
+                    onChanged: (value) {
+                      fullname = value;
+                    },
+                    decoration:
+                        const InputDecoration(labelText: 'Enter Full Name'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(13.0),
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please Phone Number must not be empty';
+                      } else {
+                        return null;
+                      }
+                    },
+                    onChanged: (value) {
+                      phoneNumber = value;
+                    },
+                    decoration:
+                        const InputDecoration(labelText: 'Enter Phone Number'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(13.0),
+                  child: TextFormField(
+                    obscureText: true,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please Password must not be empty';
+                      } else {
+                        return null;
+                      }
+                    },
+                    onChanged: (value) {
+                      password = value;
+                    },
+                    decoration: const InputDecoration(labelText: 'Password'),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    _signUpUser();
+                  },
+                  child: Container(
+                    width: MediaQuery.of(context).size.width - 40,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: Colors.yellow.shade900,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: _isLoading
+                        ? const CircularProgressIndicator(
+                            color: Colors.white,
+                          )
+                        : const Center(
+                            child: Text(
+                              'Register',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 19,
+                                  fontWeight: FontWeight.bold,
+                                  letterSpacing: 4),
+                            ),
+                          ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text('Already Have An Account?'),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return const LoginScreen();
+                            },
+                          ),
+                        );
+                      },
+                      child: const Text('Login'),
+                    )
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
