a5f1ac5ebbc5a11ba55dea5fcef7f1d89004c62a
dattht.hiday <77887747+agamamom@users.noreply.github.com>
2024-08-11 21:54:41 +0700

    update code

--- lib/views/buyers/nav_screens/cart_screen.dart
+++ lib/views/buyers/nav_screens/cart_screen.dart
@@ -5,8 +5,36 @@ class CartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Cart screen'),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Your Shopping Cart is Empty',
+              style: TextStyle(
+                  fontSize: 22, fontWeight: FontWeight.bold, letterSpacing: 5),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 40,
+              width: MediaQuery.of(context).size.width - 40,
+              decoration: BoxDecoration(
+                color: Colors.yellow.shade900,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: const Center(
+                child: Text(
+                  'CONTINUE SHOPPING',
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
