fc28b667935dff054b248b5a4c2bf65084694e4a
dattht.hiday <77887747+agamamom@users.noreply.github.com>
2024-08-14 20:50:46 +0700

    update code

--- lib/views/buyers/nav_screens/cart_screen.dart
+++ lib/views/buyers/nav_screens/cart_screen.dart
@@ -1,40 +1,40 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// class CartScreen extends StatelessWidget {
-//   const CartScreen({super.key});
+class CartScreen extends StatelessWidget {
+  const CartScreen({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             const Text(
-//               'Your Shopping Cart is Empty',
-//               style: TextStyle(
-//                   fontSize: 22, fontWeight: FontWeight.bold, letterSpacing: 5),
-//             ),
-//             const SizedBox(
-//               height: 20,
-//             ),
-//             Container(
-//               height: 40,
-//               width: MediaQuery.of(context).size.width - 40,
-//               decoration: BoxDecoration(
-//                 color: Colors.yellow.shade900,
-//                 borderRadius: BorderRadius.circular(10),
-//               ),
-//               child: const Center(
-//                 child: Text(
-//                   'CONTINUE SHOPPING',
-//                   style: TextStyle(fontSize: 18, color: Colors.white),
-//                 ),
-//               ),
-//             )
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Your Shopping Cart is Empty',
+              style: TextStyle(
+                  fontSize: 22, fontWeight: FontWeight.bold, letterSpacing: 5),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 40,
+              width: MediaQuery.of(context).size.width - 40,
+              decoration: BoxDecoration(
+                color: Colors.yellow.shade900,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: const Center(
+                child: Text(
+                  'CONTINUE SHOPPING',
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
