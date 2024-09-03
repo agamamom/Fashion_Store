e8667726a5ebe4a332c86006e3f3556eecb848d0
dattht.hiday <77887747+agamamom@users.noreply.github.com>
2024-09-03 21:34:47 +0700

    update code

---
+++ lib/vendor/views/screens/vendorProductDetail/vendor_product_detail_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class VendorProductDetailScreen extends StatelessWidget {
+  const VendorProductDetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
