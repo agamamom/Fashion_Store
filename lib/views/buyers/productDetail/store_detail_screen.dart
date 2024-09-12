fdfe041fd9e5c39c90a64e12413e0d86b0cfeb50
dattht.hiday <77887747+agamamom@users.noreply.github.com>
2024-09-12 22:35:19 +0700

    update code

--- lib/views/buyers/productDetail/store_detail_screen.dart
+++ lib/views/buyers/productDetail/store_detail_screen.dart
@@ -1,15 +1,94 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:multi_fashion_store/views/buyers/productDetail/product_detail_screen.dart';
 
 class StoreDetailScreen extends StatelessWidget {
   const StoreDetailScreen({super.key, this.storeData});
   final dynamic storeData;
   @override
   Widget build(BuildContext context) {
+    final Stream<QuerySnapshot> _productsStream =
+        FirebaseFirestore.instance.collection('products').snapshots();
     return Scaffold(
       appBar: AppBar(
         title: Text(
           storeData['bussinessName'],
         ),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: _productsStream,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Text('Something went wrong');
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Text("Loading");
+          }
+
+          return GridView.builder(
+            itemCount: snapshot.data!.docs.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: 8,
+                crossAxisSpacing: 8,
+                childAspectRatio: 200 / 300),
+            itemBuilder: (context, index) {
+              final productData = snapshot.data!.docs[index];
+              return GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return ProductDetailScreen(productData: productData);
+                      },
+                    ),
+                  );
+                },
+                child: Card(
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 170,
+                        width: 200,
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: NetworkImage(
+                                  productData['imageUrl'][0],
+                                ),
+                                fit: BoxFit.cover)),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          productData['productName'],
+                          style: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              letterSpacing: 4),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          '\$' +
+                              " " +
+                              productData['productPrice'].toStringAsFixed(2),
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              letterSpacing: 4,
+                              color: Colors.yellow.shade900),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        },
       ),
     );
   }
