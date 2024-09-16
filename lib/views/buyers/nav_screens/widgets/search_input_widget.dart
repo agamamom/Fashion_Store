e634c64f1ceb97ad433419d7470efb993eb230ef
dattht.hiday <77887747+agamamom@users.noreply.github.com>
2024-09-16 22:42:41 +0700

    update code

--- lib/views/buyers/nav_screens/widgets/search_input_widget.dart
+++ lib/views/buyers/nav_screens/widgets/search_input_widget.dart
@@ -1,35 +1,35 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_svg/svg.dart';
 
-class SeachInputWidget extends StatelessWidget {
-  const SeachInputWidget({
-    super.key,
-  });
+// class SeachInputWidget extends StatelessWidget {
+//   const SeachInputWidget({
+//     super.key,
+//   });
 
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(14.0),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(10),
-        child: TextField(
-          decoration: InputDecoration(
-            fillColor: Colors.white,
-            filled: true,
-            hintText: 'Search For Products',
-            border: const OutlineInputBorder(
-              borderSide: BorderSide.none,
-            ),
-            prefixIcon: Padding(
-              padding: const EdgeInsets.all(14),
-              child: SvgPicture.asset(
-                'assets/icons/search.svg',
-                width: 10,
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Padding(
+//       padding: const EdgeInsets.all(14.0),
+//       child: ClipRRect(
+//         borderRadius: BorderRadius.circular(10),
+//         child: TextField(
+//           decoration: InputDecoration(
+//             fillColor: Colors.white,
+//             filled: true,
+//             hintText: 'Search For Products',
+//             border: const OutlineInputBorder(
+//               borderSide: BorderSide.none,
+//             ),
+//             prefixIcon: Padding(
+//               padding: const EdgeInsets.all(14),
+//               child: SvgPicture.asset(
+//                 'assets/icons/search.svg',
+//                 width: 10,
+//               ),
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
